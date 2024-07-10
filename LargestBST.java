a95a9382eb80a2e94291e44d1c08eb110de5bf7a
Ruvaid Ahmad <118987852+Ruvaidahmad123@users.noreply.github.com>
2024-07-10 18:43:48 +0530

    Create LargestBST.java

---
+++ LargestBST.java
@@ -0,0 +1,30 @@
+class NodeValue{
+    int minnode,maxnode,maxsize;
+    NodeValue(int min,int max,int maxsize){
+        this.minnode=min;
+        this.maxnode=max;
+        this.maxsize=maxsize;
+    }
+}
+class Solution{
+    public static NodeValue findAns(Node root){
+        if(root==null){
+            return new NodeValue(Integer.MAX_VALUE,Integer.MIN_VALUE,0);
+        }
+        NodeValue left=findAns(root.left);
+        NodeValue right=findAns(root.right);
+        if(root.data>left.maxnode && root.data<right.minnode){
+            //is a BST
+            return new NodeValue(Math.min(left.minnode,root.data),Math.max(right.maxnode,root.data),left.maxsize+right.maxsize+1);
+        }
+        return new NodeValue(Integer.MIN_VALUE,Integer.MAX_VALUE,Math.max(left.maxsize,right.maxsize));
+    }
+    // Return the size of the largest sub-tree which is also a BST
+    static int largestBst(Node root)
+    {
+        // Write your code here
+        return findAns(root).maxsize;
+        
+    }
+    
+}
